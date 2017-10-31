9087a8cec6b2df9905b50fc0b96c04847b96ece1
murikubo <skyaria2017@gmail.com>
2017-10-31 13:18:17 +0900

    test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="center">
       <h1>IWJGH Project</h1>
       <hr />
-      <h5>VS Code test</h5>
+      <h5>VS Code test - TANOSHI</h5>
       <h5>I want just go home project</h5>
     </div>
 </body>
