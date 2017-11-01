de757d9d0e04748aa5812a1cd2e3e39bb60951a5
murikubo <skyaria2017@gmail.com>
2017-11-01 10:44:29 +0900

    수정수정찍

--- js/index.js
+++ js/index.js
@@ -11,8 +11,9 @@ function printClock() {
     var currentSeconds =  addZeros(currentDate.getSeconds(),2);
     var want_time = parseInt(document.getElementById('want_time').value);
 
+
     if(currentHours >= (want_time-1)){ // 본문에서 입력받은 원하는 시간 이후에 색이 빨간색으로 변함.
-        //근데 시간 기준을 알려면 오후에 해야할듯.
+        //근데 시간 기준을 알려면 오후에 해야할듯. 나중에 수정
         currentHours = '<span style="color:#de1951;">'+currentHours+'</span>'
         currentMinute= '<span style="color:#de1951;">'+currentMinute+'</span>'
         currentSeconds= '<span style="color:#de1951;">'+currentSeconds+'</span>'
