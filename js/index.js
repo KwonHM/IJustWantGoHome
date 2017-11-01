cb6f783465c17a71151e75cb313bff8d2124b03f
murikubo <skyaria2017@gmail.com>
2017-11-01 09:14:21 +0900

    5시 이후 시계 전체 빨간색+초단위 색

--- js/index.js
+++ js/index.js
@@ -15,10 +15,13 @@ function printClock() {
         currentHours = addZeros(currentHours - 12,2);
     }
 
-    if(currentHours >= 9){// 일정 시간 이후 색이 변함
+    if(currentHours >= 5){// 5시 이후 색이 변함
         currentHours = '<span style="color:#de1951;">'+currentHours+'</span>'
         currentMinute= '<span style="color:#de1951;">'+currentMinute+'</span>'
         currentSeconds= '<span style="color:#de1951;">'+currentSeconds+'</span>'
+    }
+    if(currentSeconds>=50){ // 이친구는 원래 있던 초단위 색 변함
+        currentSeconds='<span style="color:#de1951;">'+currentSeconds+'</span>'
     }
     clock.innerHTML = currentHours+":"+currentMinute+":"+currentSeconds +" <span style='font-size:50px;'>"+ amPm+"</span>"; //날짜를 출력해 줌
     
