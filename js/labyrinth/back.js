78efbcd47434d93fd0b8630b33486a48f2dd7b05
murikubo <skyaria2017@gmail.com>
2017-11-14 15:37:25 +0900

    버튼 수정

--- js/labyrinth/back.js
+++ js/labyrinth/back.js
@@ -11,3 +11,12 @@ $(document).ready(function() {
         location.href = './start.html';
     });
 }); 
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
