102856cf25038dc3e495c37dd91cfc7efc2af660
jinjeok <gudalsdl0421@gmail.com>
2017-11-15 10:28:28 +0900

    제발 document.ready는 하나안에 다 넣어라...

--- js/labyrinth/entrance.js
+++ js/labyrinth/entrance.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
     $('.anime').click(function() {
         location.href = './anime/index.html';
     });
-}); 
-
-$(document).ready(function() {
+    
     $('.idolmaster').click(function() {
         location.href = './idolmaster/start.html';
     });
