4caf9f6a988c62d24c2344ced4109ed3a47e878d
jinjeok <gudalsdl0421@gmail.com>
2017-11-15 18:22:14 +0900

    뒤로가기 수정

--- js/labyrinth/back.js
+++ js/labyrinth/back.js
@@ -14,6 +14,10 @@ $(document).ready(function() {
     $('.idolmaster_back').click(function() {
         location.href = './start.html';
     });
+
+    $('hearthstone_back').click(function() {
+        location.href = './start.html';
+    })
     
     $('#hide').click(function(){
         $('.hint').hide();
