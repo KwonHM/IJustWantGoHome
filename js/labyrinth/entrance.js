36e5def1291b521be30da5539df766db55456d92
jinjeok <gudalsdl0421@gmail.com>
2017-11-15 18:14:37 +0900

    만들면 될 거 아냐

--- js/labyrinth/entrance.js
+++ js/labyrinth/entrance.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     $('.anime').click(function() {
         location.href = './anime/index.html';
     });
-    
+
     $('.idolmaster').click(function() {
         location.href = './idolmaster/start.html';
+    });
+
+    $('.hearthstone').click(function() {
+        location.href = './hearthstone/start.html';
     });
 });
