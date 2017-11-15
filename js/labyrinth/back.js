615a036e498c98c32400020c0b69255a872a0f24
Kingjuho <ljh990627@gmail.com>
2017-11-15 13:09:01 +0900

    idolmaster_back 추가

--- js/labyrinth/back.js
+++ js/labyrinth/back.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
     $('.anime_back').click(function() {
         location.href = './index.html';
     });
+    
+    $('.idolmaster_back').click(function() {
+        location.href = './start.html';
+    });
+    
     $('#hide').click(function(){
         $('.hint').hide();
     });
