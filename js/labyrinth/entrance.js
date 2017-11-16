2c3ef263ec9e6deb836cf6acead791bb5ca5a966
jinjeok <gudalsdl0421@gmail.com>
2017-11-16 15:16:37 +0900

    포켓몬 버튼 생성

--- js/labyrinth/entrance.js
+++ js/labyrinth/entrance.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
     $('.hearthstone').click(function() {
         location.href = './hearthstone/start.html';
     });
+
+    $('.pokemon').click(function() {
+        location.href = './pokemon/start.html';
+    });
+
    // $('.yggdrasil').click(function() {
    //     location.href = './yggdrasil/1st_floor.html'; 
    //이걸 언젠가 메인에 박아둘 수도 있고 아닐 수도 있고. 아직은 안 쓰임
