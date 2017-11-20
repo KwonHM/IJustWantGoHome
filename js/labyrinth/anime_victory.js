87ad1b9b5ed9794d10dcebac7ec10e14dc229296
jinjeok <gudalsdl0421@gmail.com>
2017-11-20 12:01:19 +0900

    github page 불가 else 문에 preventDefault 추가

--- js/labyrinth/anime_victory.js
+++ js/labyrinth/anime_victory.js
@@ -8,6 +8,7 @@ function save_score() {
         event.preventDefault();
     } else {
         alert('Github page에서 자체적으로 서버측 언어를 지원하지 않기 때문에 사용할 수 없습니다. 공식 홈페이지를 사용해주세요.');
+        event.preventDefault();
     } //서버에 올릴때 else문 지워주세용
     $.ajax({
         url:'../../php/save_score.php',
