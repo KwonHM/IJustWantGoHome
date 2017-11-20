5cf5352d09d5310a70056363c933d27c218167cc
jinjeok <gudalsdl0421@gmail.com>
2017-11-20 11:59:50 +0900

    github page 사용불가 메시지 추가

--- js/labyrinth/anime_victory.js
+++ js/labyrinth/anime_victory.js
@@ -6,7 +6,9 @@ function save_score() {
     if(document.saveinfo.user_name.value.length == 0){
         alert('이름을 입력해 주세요.');
         event.preventDefault();
-    }
+    } else {
+        alert('Github page에서 자체적으로 서버측 언어를 지원하지 않기 때문에 사용할 수 없습니다. 공식 홈페이지를 사용해주세요.');
+    } //서버에 올릴때 else문 지워주세용
     $.ajax({
         url:'../../php/save_score.php',
         type:'post',
