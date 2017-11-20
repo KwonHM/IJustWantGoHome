fd6ceeca92d8f7503b50bfdf087b01621138422e
lunch1019 <30451760+lunch1019@users.noreply.github.com>
2017-11-20 12:30:37 +0900

    Add files via upload
    왜 안생겨 ㅡㅡ

---
+++ forserver/anime_victory.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#saveinfo').submit(save_score);
+});
+
+function save_score() { 
+    if(document.saveinfo.user_name.value.length == 0){
+        alert('이름을 입력해 주세요.');
+        event.preventDefault();
+    } 
+    $.ajax({
+        url:'../../php/save_score.php',
+        type:'post',
+        data:$('#saveinfo').serialize(),
+    }).done(function(data) {
+        if(data == 1){
+            alert('저장이 완료 되었습니다.');
+            location.href='../../php/leader_board.php';
+        }else {
+            alert(data);
+        }
+    })
+} 
